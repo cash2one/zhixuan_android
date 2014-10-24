821cef871e91d132041c8c53c2b839c7bcd4f164
lei286394973 <286394973@qq.com>
2014-10-24 20:25:36 +0800

    change tab to space

--- src/com/zhixuan/utils/ZXSharedPreferences.java
+++ src/com/zhixuan/utils/ZXSharedPreferences.java
@@ -13,105 +13,105 @@ import com.android.volley.toolbox.Volley;
 
 public class ZXSharedPreferences {
 
-	private final String SP_NAME = "ZHIXUAN";
-	private final String SP_CITY_NAME = "CITY_NAME";
-	private final String SP_CITY_ID = "CITY_ID";
-	private final String SP_PROVINCE_AND_CITY = "PROVINCE_AND_CITY";
-	private final String SP_LAST_CHECK_UPDATE = "LAST_CHECK_UPDATE";
-	private final String DEFAULT_LAST_CHECK_UPDATE = "2014-10-23";
-	private final String DEFAULT_CITY_NAME = "成都市";
-	private final String DEFAULT_CITY_ID = "1974";
+    private final String SP_NAME = "ZHIXUAN";
+    private final String SP_CITY_NAME = "CITY_NAME";
+    private final String SP_CITY_ID = "CITY_ID";
+    private final String SP_PROVINCE_AND_CITY = "PROVINCE_AND_CITY";
+    private final String SP_LAST_CHECK_UPDATE = "LAST_CHECK_UPDATE";
+    private final String DEFAULT_LAST_CHECK_UPDATE = "2014-10-23";
+    private final String DEFAULT_CITY_NAME = "成都市";
+    private final String DEFAULT_CITY_ID = "1974";
 
-	private RequestQueue mQueue;
-	private SharedPreferences mSharedPreferences;
+    private RequestQueue mQueue;
+    private SharedPreferences mSharedPreferences;
 
-	public ZXSharedPreferences(Context context) {
-		mQueue = Volley.newRequestQueue(context);
-		mSharedPreferences = context.getSharedPreferences(SP_NAME,
-				context.MODE_PRIVATE);
-	}
+    public ZXSharedPreferences(Context context) {
+        mQueue = Volley.newRequestQueue(context);
+        mSharedPreferences = context.getSharedPreferences(SP_NAME,
+                context.MODE_PRIVATE);
+    }
 
-	/*
-	 * 获取城市名字
-	 */
-	public String getCityName() {
-		return mSharedPreferences.getString(SP_CITY_NAME, DEFAULT_CITY_NAME);
-	}
+    /*
+     * 获取城市名字
+     */
+    public String getCityName() {
+        return mSharedPreferences.getString(SP_CITY_NAME, DEFAULT_CITY_NAME);
+    }
 
-	/*
-	 * 设置城市名字
-	 */
-	public void setCityName(String cityName) {
-		Editor editor = mSharedPreferences.edit();
-		editor.putString(SP_CITY_NAME, cityName);
-		editor.commit();
-	}
+    /*
+     * 设置城市名字
+     */
+    public void setCityName(String cityName) {
+        Editor editor = mSharedPreferences.edit();
+        editor.putString(SP_CITY_NAME, cityName);
+        editor.commit();
+    }
 
-	/*
-	 * 获取城市id
-	 */
-	public String getCityId() {
-		return mSharedPreferences.getString(SP_CITY_ID, DEFAULT_CITY_ID);
-	}
+    /*
+     * 获取城市id
+     */
+    public String getCityId() {
+        return mSharedPreferences.getString(SP_CITY_ID, DEFAULT_CITY_ID);
+    }
 
-	/*
-	 * 设置城市id
-	 */
-	public void setCityId(String cityId) {
-		Editor editor = mSharedPreferences.edit();
-		editor.putString(SP_CITY_ID, cityId);
-		editor.commit();
-	}
+    /*
+     * 设置城市id
+     */
+    public void setCityId(String cityId) {
+        Editor editor = mSharedPreferences.edit();
+        editor.putString(SP_CITY_ID, cityId);
+        editor.commit();
+    }
 
-	/*
-	 * 本地是否已经有省份和城市的信息
-	 */
-	public boolean hasProvinceAndCity() {
-		return getProvinceAndCityFromLocal() == "none" ? false : true;
-	}
+    /*
+     * 本地是否已经有省份和城市的信息
+     */
+    public boolean hasProvinceAndCity() {
+        return getProvinceAndCityFromLocal() == "none" ? false : true;
+    }
 
-	/*
-	 * 本地加载城市
-	 */
-	public String getProvinceAndCityFromLocal() {
-		return mSharedPreferences.getString(SP_PROVINCE_AND_CITY, "none");
-	}
+    /*
+     * 本地加载城市
+     */
+    public String getProvinceAndCityFromLocal() {
+        return mSharedPreferences.getString(SP_PROVINCE_AND_CITY, "none");
+    }
 
-	public String getLastCheckUpdate() {
-		return mSharedPreferences.getString(SP_LAST_CHECK_UPDATE,
-				DEFAULT_LAST_CHECK_UPDATE);
-	}
+    public String getLastCheckUpdate() {
+        return mSharedPreferences.getString(SP_LAST_CHECK_UPDATE,
+                DEFAULT_LAST_CHECK_UPDATE);
+    }
 
-	public void setLastCheckUpdate(String strDate) {
-		Editor editor = mSharedPreferences.edit();
-		editor.putString(SP_LAST_CHECK_UPDATE, strDate);
-		editor.commit();
-	}
+    public void setLastCheckUpdate(String strDate) {
+        Editor editor = mSharedPreferences.edit();
+        editor.putString(SP_LAST_CHECK_UPDATE, strDate);
+        editor.commit();
+    }
 
-	/*
-	 * 请求服务端加载城市信息
-	 */
-	public void getProvinceAndCityFromServer() {
+    /*
+     * 请求服务端加载城市信息
+     */
+    public void getProvinceAndCityFromServer() {
 
-		StringRequest stringRequest = new StringRequest(Consts.MAIN_DOMAIN
-				+ "/kaihu/api_get_province_and_city",
-				new Response.Listener<String>() {
-					@Override
-					public void onResponse(String response) {
-						// 存入数据
-						Editor editor = mSharedPreferences.edit();
-						editor.putString(SP_PROVINCE_AND_CITY, response);
-						editor.putString(SP_CITY_ID, DEFAULT_CITY_ID);
-						editor.putString(SP_CITY_NAME, DEFAULT_CITY_NAME);
-						editor.commit();
-					}
-				}, new Response.ErrorListener() {
-					@Override
-					public void onErrorResponse(VolleyError error) {
-						Log.e("TAG", error.getMessage(), error);
-					}
-				});
+        StringRequest stringRequest = new StringRequest(Consts.MAIN_DOMAIN
+                + "/kaihu/api_get_province_and_city",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // 存入数据
+                        Editor editor = mSharedPreferences.edit();
+                        editor.putString(SP_PROVINCE_AND_CITY, response);
+                        editor.putString(SP_CITY_ID, DEFAULT_CITY_ID);
+                        editor.putString(SP_CITY_NAME, DEFAULT_CITY_NAME);
+                        editor.commit();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("TAG", error.getMessage(), error);
+                    }
+                });
 
-		mQueue.add(stringRequest);
-	}
+        mQueue.add(stringRequest);
+    }
 }
